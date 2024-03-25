8241cc46ac2923d26461031c666ccd6d1d10c9a9
aladdin4u <taofikakinkumi@gmail.com>
2024-03-25 14:16:06 +0100

    add dotenv

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -59,7 +59,7 @@ const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
   const { repositories } = useRepositories();
-  console.log(repositories);
+  // console.log(repositories);
 
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,9 +1,10 @@
+import Constants from "expo-constants";
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: 'http://127.0.0.1:4000/graphql',
+    uri: Constants.expoConfig.extra.apollo,
     cache: new InMemoryCache(),
   });
 };
